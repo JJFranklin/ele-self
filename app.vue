2b1ae8a3ccf5c6b87f4b238bc49bc86a2fbfa195
franklinhe <franklinhe@yunify.com>
2018-08-30 18:51:20 +0800

    添加修改eleme 的页面

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    import "./src/scss/app.scss";
+    export default {
+        data() {
+            return {
+                msg: "hello wrrld"
+            }
+        },
+        co
+    }
+</script>
